2799cf124198981f2b08f472b5790606c59bf7b0
Ahnad Fauzan <fauzan18296@gmail.com>
2025-12-30 21:34:19 +0700

    add: Change code on exercise-2

--- Exercise/exercise-2.py
+++ Exercise/exercise-2.py
@@ -3,7 +3,5 @@ found_char_num = ""
 for found_number in letter:
           if found_number.isdigit():
                     found_char_num += found_number
-          else:
-                    print("Tidak ditemukan digit pada hasil inputan kalimat!")
 found_result = int(found_char_num)
 print(found_char_num)
