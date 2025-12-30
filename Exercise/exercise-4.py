fb52cf60b2b9a4dee0d6c4f0d460b8ceb694230e
Ahnad Fauzan <fauzan18296@gmail.com>
2025-12-30 21:37:02 +0700

    add: Change code on exercise-4 and exercise-5

--- Exercise/exercise-4.py
+++ Exercise/exercise-4.py
@@ -3,5 +3,5 @@ with open("gabungan.txt", "w") as outfile:
           for filename in filenames:
                     with open(filename) as infile:
                               for letter in infile:
-                                        if outfile.write(letter):
-                                                  print("File gabungan telah berhasil di buat!!")
+                                        outfile.write(letter)
+                                        print("File gabungan.txt telah berhasil di buat!!")
--- Exercise/exercise-5.py
+++ Exercise/exercise-5.py
@@ -2,4 +2,4 @@ import datetime as dt
 date = dt.datetime.now()
 with open("datenotes.txt", "a") as file:
           file.write(f"{date.strftime("%d-%m-%Y, %H:%M:%S")}\n")
-          print("Waktu dan tanggal telah berhasil dibuat pada file datenotes!!")
+          print("Waktu dan tanggal telah berhasil dibuat pada file datenotes.txt!!")
